2449ca162a7da6d731ed8814dad23391012c871e
Danila Stepovoy <danilastepovois@gmail.com>
2021-12-18 15:38:16 +0300

    Change Level 1

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+   
+     void Update()
     {
         
     }
