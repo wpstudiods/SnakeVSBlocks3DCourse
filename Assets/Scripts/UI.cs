dd94ea15c948dad7fe5a2541ad0e92530b50adaa
Danila Stepovoy <danilastepovois@gmail.com>
2021-12-22 20:09:29 +0300

    Added TitleScreen and Level Selector

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UI : MonoBehaviour
+{
+    public void PressStart()
+    {
+        SceneManager.LoadScene("Level_Select");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void NextLevel1()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void NextLevel2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+    
+    public void NextLevel3()
+    {
+        SceneManager.LoadScene("Level_3");
+    }
+    
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
